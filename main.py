7c3a03cd0b11b67fc01c08aa963e71d5bcb5ac60
naumanxds <nauman.nasir919@gmail.com>
2019-11-24 06:20:49 +0500

    use selenium and add readme

--- main.py
+++ main.py
@@ -1,156 +1,163 @@
-import requests
-import json
-import urllib.request
-import time
 import csv
 
 from datetime import datetime
+from selenium import webdriver
 from bs4 import BeautifulSoup
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.options import Options
 
 # constants used in code
 NOT_FOUND = 'None'
 INCREMENT_ONE = 1
-SLEEP_SEC = 2
+BASE_URL = 'https://www.daraz.pk'
 
 # create file with time attached to it for safty purposes
 fHandle = open('csvFileCreatedAt-' + datetime.now().strftime('%H-%M-%S') + '.csv', 'w', encoding='utf-8')
 
+# create browser instance
+manager = GeckoDriverManager()
+browserOptions = Options()
+browserOptions.add_argument("--headless")
+driver = webdriver.Chrome(executable_path=manager.install(), options=browserOptions)
+
+# get html of the provided page url
+def getHtml(url):
+    try:
+        driver.get(url)
+        driver.execute_script('return document.documentElement.outerHTML')
+        return BeautifulSoup(driver.page_source, 'html.parser')
+
+    except Exception as e:
+        print('     >> Error in Fetching HTML from Url => ' + url)
+        print('     >> ERRROR => ' + format(e))
+
+    return False
+
 # write in file
 def writeFile(data, url = ''):
-	try:
-		csvWriter = csv.writer(fHandle)
-		csvWriter.writerow(data)
-	except Exception as e:
-		print('		>> Error in Writing Data into the file => ' + url)
-		print('		>> ERRROR => ' + format(e))
-
-# get html of the provided url page
-def getHtml(url):
-	try:
-		response = requests.get(url)
-		return BeautifulSoup(response.text, 'html.parser')
-	except Exception as e:
-		print('		>> Oops! Something went worng in fetching the link - ' + format(e))
-
-	return False
+    try:
+        csvWriter = csv.writer(fHandle)
+        csvWriter.writerow(data)
+    except Exception as e:
+        print('     >> Error in Writing Data into the file => ' + url)
+        print('     >> ERRROR => ' + format(e))
 
 # iterate through the fetched links get data
-def iterateLinks(subLinks):
-	for l in subLinks:
-		data = []
-		html = getHtml(l['url'])
-		time.sleep(SLEEP_SEC)
-		try:
-			# product name
-			fetched = html.find('span', {'class' : 'pdp-mod-product-badge-title'})
-			if str(fetched) != NOT_FOUND:
-				data.append(fetched.get_text().strip())
-			else:
-				data.append('PRODUCT NAME NOT FOUND')
+def iterateLinks(links):
+    for link in links:
+        html = getHtml(link)
+        # try:
+            # product name
+        title = html.find('span', {'class' : 'pdp-mod-product-badge-title'})
+        if str(title) != NOT_FOUND:
+            title = title.get_text().strip()
+        else:
+            title = 'Title not found'
 
-			# prodct price
-			fetched = html.find('span', {'class' : 'pdp-price pdp-price_type_normal pdp-price_color_orange pdp-price_size_xl'})
-			if str(fetched) != NOT_FOUND:
-				data.append(fetched.get_text().split('Rs. ')[1])
-			else:
-				data.append('PRICE NOT FOUND')
+        # prodct price
+        price = html.find('span', {'class' : 'pdp-price pdp-price_type_normal pdp-price_color_orange pdp-price_size_xl'})
+        if str(price) != NOT_FOUND:
+            price = price.get_text().split('Rs. ')[1]
+        else:
+            price = 'Price not found'
 
-			# breadcrums
-			breadcrum = ''
-			fetched = html.find_all('span', {'class' : 'breadcrumb_item_text'})
-			if str(fetched) != NOT_FOUND:
-				for l in fetched:
-					breadcrum += ' > ' + l.find('span').get_text().strip()
-			else:
-				breadcrum = 'BREADCRUMS NOT FOUND'
+        # breadcrums
+        breadcrums = ''
+        breadcrum = html.find_all('span', {'class' : 'breadcrumb_item_text'})
+        print((html.find('ul', {'id' : 'J_breadcrumb'})).text(separator=' > ', strip='\n'))
+        exit()
+        if str(breadcrum) != NOT_FOUND:
+            for l in breadcrum:
+                breadcrum += ' > ' + l.find('span').get_text().strip()
+        else:
+            breadcrum = 'Breadcrums not found'
 
-			data.append(breadcrum)
+        # details points
+        detail = html.find('div', {'class' : 'html-content pdp-product-highlights'})
+        if str(detail) != NOT_FOUND:
+            detail = detail.find_all('li')
+            for l in detail:
+                detail += ' - ' + l.get_text() + '\n'
+        else:
+            detail = 'Product Description points not found'
 
-			# details points
-			details = ''
-			fetched = html.find('div', {'class' : 'html-content pdp-product-highlights'})
-			if str(fetched) != NOT_FOUND:
-				fetched = fetched.find_all('li')
-				for l in fetched:
-					details += ' - ' + l.get_text() + '\n'
-			else:
-				details = 'PRODUCT DESCRIPTION POINTS NOT FOUND'
+        # product descripton
+        descripton = html.find('div', {'class' : 'html-content detail-content'})
+        if str(descripton) != NOT_FOUND:
+            descripton = descripton.get_text()
+        else:
+            descripton = 'Product Detail Description not found'
 
-			data.append(details)
+        # product specifications
+        specification = html.find_all('li', {'class' : 'key-li'})
+        if str(specification) != NOT_FOUND:
+            for l in specification:
+                specification += l.find('span').get_text() + ' : ' + l.find('div').get_text() + '\n'
+        else:
+            specification = 'Product Specifications not found'
 
-			# product descripton
-			fetched = html.find('div', {'class' : 'html-content detail-content'})
-			if str(fetched) != NOT_FOUND:
-				data.append(fetched.get_text())
-			else:
-				data.append('PRODUCT DETAIL DESCRIPTION NOT FOUND')
+        # box content
+        boxContent = html.find('div', {'class' : 'box-content'})
+        if str(boxContent) != NOT_FOUND:
+            boxContent = boxContent.find('span').get_text() + ' : ' + boxContent.find('div').get_text()
+        else:
+            boxContent = 'Box Content not found'
 
-			# product specifications
-			specKey = ''
-			fetched = html.find_all('li', {'class' : 'key-li'})
-			if str(fetched) != NOT_FOUND:
-				for l in fetched:
-					specKey += l.find('span').get_text() + ' : ' + l.find('div').get_text() + '\n'
-			else:
-				specKey = 'PRODUCT SPECIFICATIONS NOT FOUND'
+        # product pictures
+        pics = html.find_all('img', {'class' : 'pdp-mod-common-image item-gallery__thumbnail-image'})
+        images = []
+        if str(pics) != NOT_FOUND:
+            for l in pics:
+                iamges.append(l.get('src').split('.jpg_')[0] + '.jpg_400x400.jpg')
+        else:
+            images = ['IMAGES NOT FOUND']
 
-			data.append(specKey)
-
-			# box content
-			boxContent = html.find('div', {'class' : 'box-content'})
-			if str(boxContent) != NOT_FOUND:
-				data.append(boxContent.find('span').get_text() + ' : ' + boxContent.find('div').get_text())
-			else:
-				data.append('BOX CONTENT NOT FOUND')
-
-			# product pictures
-			pics = html.find_all('img', {'class' : 'pdp-mod-common-image item-gallery__thumbnail-image'})
-			if str(pics) != NOT_FOUND:
-				for l in pics:
-					data.append(l.get('src').split('.jpg_')[0] + '.jpg_400x400.jpg')
-			else:
-				data.append('IMAGES NOT FOUND')
-
-			# write data in file
-			writeFile(data)
-		except Exception as e:
-			print('		>> Entry missed due to some error from this link => ' + l['url'])
-			print(' 	===================================================')
-			print('		>> ERRROR => ' + format(e))
+        # write data in file
+        writeFile([title, price, breadcrum, detail, descripton, specification, boxContent] + images)
+        print('         ** Product Done => ' + str(link))
+        # except Exception as e:
+        #     print('     >> Entry missed due to some error from this link => ' + str(link))
+        #     print('     >> ERRROR => ' + format(e))
 
 
 # input for user
-enteredUrl = input('Please Enter Starting Point for Scrapper: ')
-startUrl = enteredUrl.split('&page=')[0]
+siteUrl = input('Please Enter Starting Point for Scrapper: ')
 print('=== Starting Scrapping ===')
 writeFile([
-	'Product Name',
-	'Product Price',
-	'Bread Crums',
-	'Detail Points',
-	'Product Descripton',
-	'Product Specifications',
-	'Box Content',
-	'Images'
+    'Product Name',
+    'Product Price',
+    'Bread Crums',
+    'Detail Points',
+    'Product Descripton',
+    'Product Specifications',
+    'Box Content',
+    'Images'
 ])
+
 try:
-	count = enteredUrl.split('&page=')[1]
-	count = int(count.split('&')[0])
+    count = siteUrl.split('&page=')[1]
+    count = int(count.split('&')[0]) + 1
 except:
-	count = 1
+    count = 2
 
-while count <= 500:
-	html = getHtml(startUrl + '&page=' + str(count))
-	time.sleep(SLEEP_SEC)
-	parsedJson = html.find_all('script', {'type':'application/ld+json'})
-	links = (json.loads(parsedJson[1].text))['itemListElement']
-	if not links:
-		break
+while True:
+    html = getHtml(siteUrl)
+    link = html.find_all('div', {'class' : 'c16H9d'})
+    links = []
+    for loop in link:
+        links.append('https:' + loop.find('a').get('href'))
 
-	iterateLinks(links)
-	print(str(count) + ' == Pages Done')
-	count += INCREMENT_ONE
+    iterateLinks(links)
+    exit()
+    print(' >> Page Done >> ' + str(count - 1) + ' Done URL => ' + siteUrl)
+    siteUrl = html.find('li', {'title' : count})
+    if str(link) == NOT_FOUND or str(siteUrl) == NOT_FOUND:
+        break
+
+    siteUrl = BASE_URL + siteUrl.find('a').get('href')
+    count += 1
 
 # close file
 fHandle.close()
+driver.quit()
 print('=== Scrapping Finished ===')
